6784d1bcd6286feafb6cf2112894f028b7f94d3b
Chanya <chanyasrisasp@gmail.com>
2024-09-12 14:52:36 +0200

    internalization working

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <template>
-<h1>Kowiste</h1>
+<h1 class="bg-warning">Kowiste</h1>
 <h1>{{ $t('message.hello') }}</h1>
-<select @change="changeLanguage($event)" v-model="currentLocale">
+<select @change="changeLanguage" v-model="currentLocale">
       <option value="en">English</option>
       <option value="es">Spanish</option>
       <option value="th">Thai</option>
     </select>
-  <div id="app">
+  <div >
     <button class="btn btn-warning" @click="toggleTheme">Change Theme</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-
+import { onMounted ,ref} from 'vue';
+import { useI18n } from "vue-i18n"
+const currentLocale=ref('en')
+const {locale} = useI18n();
 let isDark= false
 function   toggleTheme() {
       isDark = !isDark;
       if (isDark) {
         console.log('dark');
-        // document.body.setAttribute('theme','light')
-        document.body.setAttribute('theme','dark')
+       document.body.classList.add('dark-theme')
+       document.body.classList.remove('light-theme')
       } else {
         console.log('light');
-        document.body.setAttribute('theme','light')
-        // document.body.setAttribute('theme','light')
-        // document.body.classList.add('dark-theme')
-        // document.body.classList.remove('light-theme')
+        
+        document.body.classList.add('light-theme')
+        document.body.classList.remove('dark-theme')
       }
     }
     
-function changeLanguage(event){ 
-  this.$i18n.locale = event.target.value;
-
+function changeLanguage(){ 
+  locale.value = currentLocale.value
 }
 onMounted(()=>{
-  document.body.setAttribute('theme','light');
+  document.body.classList.add('light-theme')
 })
 
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createI18n  } from 'vue-i18n'
+import { ThemeBsPlugin } from '@/plugins/theme/init'
+import type { IThemeOption } from './plugins/theme/model'
 
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
 
-import 'bootstrap'
+
 import en from './assets/locales/en.json';
 import es from './assets/locales/es.json';
 import th from './assets/locales/th.json';
 const app = createApp(App)
-
+const themeOption = {} as IThemeOption
 const i18n = createI18n({
     locale: 'en', // Set the default locale
     fallbackLocale: 'en', // Set the fallback locale
@@ -25,6 +27,7 @@ const i18n = createI18n({
 app.use(i18n);
 app.use(createPinia())
 app.use(router)
+app.use(ThemeBsPlugin, themeOption)
 
 
 app.mount('#app')
