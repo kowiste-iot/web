4c66588be827422793b17de3cb4fc1bca47d5909
Chanya <chanyasrisasp@gmail.com>
2024-11-26 15:46:33 +0100

    unfinished text

--- src/views/dashboard/card/Text.vue
+++ src/views/dashboard/card/Text.vue
@@ -3,7 +3,15 @@
     :data="data"
     :measureCondition="modelValue == data.options.conditionText"
   >
-    <div>{{ modelValue }}</div>
+    <div ref="parent" class="h-100 w-100 flex items-center justify-center">
+      <div
+        ref="textRef"
+        class="font-bold text-center"
+        :style="{ fontSize: `${fontSize}px` }"
+      >
+       {{ modelValue }}
+      </div>
+    </div>
   </WidgetCard>
 </template>
 
@@ -29,6 +37,64 @@ const modelValue = defineModel<string>();
 // methods
 // lifeCycle
 // watch
+import { ref, onMounted, nextTick,onUpdated } from 'vue';
+
+const textRef = ref<HTMLDivElement | null>(null);
+const parent = ref<HTMLDivElement | null>(null);
+const fontSize = ref(100);
+
+async function adjustTextSize() {
+  await nextTick();
+  console.log('sdf');
+
+  if (!textRef.value) return;
+
+  const text = textRef.value;
+  const container = parent.value;
+
+  if (!container) return;
+
+  fontSize.value = 0;
+  // Container dimensions
+  const containerWidth = container.clientWidth;
+  const containerHeight = container.clientHeight;
+
+  if (containerWidth <= 0 || containerHeight <= 0) {
+    return;
+  }
+  let count = 0;
+  while (true) {
+    await nextTick();
+    count++;
+    if (count > 500) break;
+    if (
+      text.clientWidth >= containerWidth ||
+      text.clientHeight >= containerHeight
+    ) {
+      console.log('there',count,text.clientHeight, containerHeight,fontSize.value)
+      break;
+    } else {
+      fontSize.value++;
+      console.log('here',count,text.clientHeight, containerHeight,fontSize.value);
+    }
+  }
+}
+
+onMounted(() => adjustTextSize());
+onUpdated(() => {
+  adjustTextSize();
+});
+
+
+
+
 </script>
 
-<style scoped></style>
+<style scoped>
+div {
+  overflow: hidden; /* Prevent overflow */
+  display: flex;
+  align-items: center; /* Vertically center the text */
+  justify-content: center; /* Horizontally center the text */
+}
+</style>
--- src/views/dashboard/card/TextForm.vue
+++ src/views/dashboard/card/TextForm.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-md-6">
       <div class="form-group row justify-content-center px-5 my-4">
-        <Text :data="model.data"  v-model="measure"/>
+        <Text :data="model.data" v-model="measure" />
       </div>
       <div class="form-group row justify-content-center">
         <label class="col-md-4">
@@ -63,41 +63,6 @@
             type="checkbox"
             role="switch"
             v-model="model.data.options.trueEmotion"
-          />
-        </div>
-      </div>
-      <div class="form-group row justify-content-center mb-3">
-        <label class="col-md-4"> {{ $t('widget.bool.form.showText') }} </label>
-        <div class="col-md-8 form-switch">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            role="switch"
-            v-model="model.data.options.showText"
-          />
-        </div>
-      </div>
-      <div v-if="model.data.options.showText">
-        <div class="form-group row justify-content-center mb-1">
-          <label class="col-md-4 col-form-label">
-            {{ $t('widget.bool.form.textTrue') }}
-          </label>
-          <Input
-            class="col-md-8 col-form-label"
-            placeholder=""
-            type="text"
-            v-model="model.data.options.trueText"
-          />
-        </div>
-        <div class="form-group row justify-content-center mb-3">
-          <label class="col-md-4 col-form-label">
-            {{ $t('widget.bool.form.textFalse') }}
-          </label>
-          <Input
-            class="col-md-8 col-form-label"
-            placeholder=""
-            type="text"
-            v-model="model.data.options.falseText"
           />
         </div>
       </div>
